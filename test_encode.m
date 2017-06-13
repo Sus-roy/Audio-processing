44cf37d70e6134b39a8580bbecff17dabe1f4f9c
Sus-roy <suravi.s.roy@gmail.com>
2017-06-13 21:31:12 +0300

    Create test_encode.m

--- test_encode.m
+++ test_encode.m
@@ -1,37 +1,25 @@
-function recording = encode(codeLength)
+function recording = test_encode(codeLength)
 if nargin == 0
     codeLength = 16;
 end
 code = randi([0,1],codeLength,1);
-%preAmble = [1 0 0 1 0 1];
 recording = [];
-idleTime = 0.04 %40ms;
-preAmble = upChirp(0,200,0.1*44100) + idleTime;
-recording = [recording,preAmble];
-
+idleTime = zeros(1,0.04*44100);
+preAmble= upChirp(0,200,44100,0.1);
+recording = [preAmble,idleTime];
 for j=1:length(code)
-    
     if code(j) == 1
-        y = upChirp() + idleTime;
+        y = upChirp(0,200,44100,0.02);
     elseif code(j) == 0
-        y = downChirp() + idleTime;
+        y = downChirp(200,0,44100,0.02); 
     end
-    recording = [recording,y];    
+    recording = [recording, y, idleTime];    
 end
 
-%plotting the original recording and revrese recording
-size(recording)  
-reverse_recording= fliplr(recording);
+size(recording);
 figure
 subplot(2,1,1)
 plot (recording)
 title('original recording')
-
-subplot(2,1,2)
-plot (reverse_recording)
- 
-title('reverse recording')
-%sound(recording)
-%sound(reverse_recording)
-
+sound(recording)
 end
