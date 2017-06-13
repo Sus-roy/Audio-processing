41463ca4fc2dc21f13c470be7dc4f2324d2b10af
Sus-roy <suravi.s.roy@gmail.com>
2017-06-13 21:32:40 +0300

    Create upChirp.m

--- upChirp.m
+++ upChirp.m
@@ -1,18 +1,14 @@
-function [ y1 ] = upChirp( f_low,f_high,sample_rate )
-%UNTITLED7 Summary of this function goes here
-%   Detailed explanation goes here
-%clear variables
+function [ y1 ] = up( f_low,f_high,sample_rate,time )
 if nargin == 0
     f_low = 0;
-    f_high = 800;
+    f_high = 200;
     sample_rate =44100;
+    time=100;
 end
 fs = sample_rate;
 f1 = f_low;
 fh = f_high;
-n = 0:1/fs:1;
-phi = 2*pi*(f1*n + (fh-f1)*n.*n/2);
-y1 = 0.5*sin(phi);
-sound(y1,fs);
+t=time;
+t1=linspace(0,t,t*fs);
+y1= chirp(t1,f1,t,fh);
 end
-
