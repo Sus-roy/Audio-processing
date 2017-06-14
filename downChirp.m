70676d8be181c01e3fb9aa49bda97b41fc41c9ec
Sus-roy <suravi.s.roy@gmail.com>
2017-06-14 03:45:55 +0300

    Create downChirp.m

--- downChirp.m
+++ downChirp.m
@@ -1,14 +1,18 @@
-function [ y1 ] = up( f_low,f_high,sample_rate,time )
+function [  x ] = downChirp( f_low,f_high,sample_rate,time )
 if nargin == 0
     f_low = 200;
     f_high = 0;
     sample_rate =44100;
-    time=10;
+    time=1;
 end
 fs = sample_rate;
 f1 = f_low;
 fh = f_high;
 t=time;
 t1=linspace(0,t,t*fs);
-y1= chirp(t1,f1,t,fh);
+t0=t1(1);
+T=t-t0;
+k=(fh-f1)/T;
+x=cos(2*pi*(k/2*t1+f1).*t1);
+%plot(t1,x);
 end
