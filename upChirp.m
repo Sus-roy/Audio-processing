848d7f4d65cbd0ccebf1a0c349b304aaea01f213
Sus-roy <suravi.s.roy@gmail.com>
2017-06-14 03:44:38 +0300

    Create upChirp.m

--- upChirp.m
+++ upChirp.m
@@ -1,14 +1,18 @@
-function [ y1 ] = up( f_low,f_high,sample_rate,time )
+function [  x ] = upChirp( f_low,f_high,sample_rate,time )
 if nargin == 0
     f_low = 0;
     f_high = 200;
     sample_rate =44100;
-    time=100;
+    time=1;
 end
 fs = sample_rate;
 f1 = f_low;
 fh = f_high;
 t=time;
 t1=linspace(0,t,t*fs);
-y1= chirp(t1,f1,t,fh);
+t0=t1(1);
+T=t-t0;
+k=(fh-f1)/T;
+x=cos(2*pi*(k/2*t1+f1).*t1);
+%plot(t1,x);
 end
