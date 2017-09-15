fa0b788cc40f5428b96a568dec33c368a630fd81
Petteri Nurmi <ptnurmi@cs.helsinki.fi>
2017-09-16 01:32:44 +0300

    another iteration

--- runTests.m
+++ runTests.m
@@ -9,13 +9,16 @@ for i=1:length(files)
     
     [X,fs] = readFile(files{i});
     
+    m = max(X);
+    X = X .* m^-1;
+    
     % Uncomment following block to search for offset
-%     for q=1:882        
-%         testcode = decodeContents(X,fs,0,q);
-%         if sum(abs(testcode - code)) == 0
-%             fprintf('Offset %d, error %d\n', q, sum(abs(testcode - code)));
-%         end
-%     end    
+    for q=1:1024       
+         testcode = decodeContents(X,fs,0,q);
+         if sum(abs(testcode - code)) == 0
+             fprintf('Offset %d, error %d\n', q, sum(abs(testcode - code)));
+         end
+     end    
 
     % Test for a given file
     % Parameters: 
@@ -23,9 +26,12 @@ for i=1:length(files)
     % fs: sampling rate
     % 0/1: whether to plot results or not, 1 = yes, 0 = no
     % offset: start of signal to ignore
-    testcode = decodeContents(X,fs,1,38);
+%    testcode = decodeContents(X,fs,1,38);
+ %   testcode
+  %  code
     
-    %testcode = decodeContents(X,fs,1,38);
+    testcode = decodeContents(X,fs,1,138);
+    [testcode; code]
     
     
 end
@@ -43,7 +49,7 @@ end
 codeLength = 8;
 
 % Determine 
-winSize = fs * 0.02;
+winSize = 1024;%fs * 0.02;
 nfft = 2^nextpow2(winSize);
 fftBins = fs/2 * linspace(0,1,nfft/2);
 
@@ -67,7 +73,7 @@ if plotOn > 0
     subplot(4,1,2);
     hold on;
     subplot(4,1,3);
-    plot(q:length(data),data(q:end),'k--');
+    plot(offset:length(data),data(offset:end),'k--');
     hold on
     subplot(4,1,4);
     hold on
@@ -80,20 +86,24 @@ for i = offset:winSize:length(data)
     end
     
     % Extract next window
-    tmp = data(i:i+winSize-1);
+    tmp = data(i:i+winSize-1).*2;
        
-    
     % Add padding to the signal and perform bandpass filtering
     tmp = Utils.addPadding(tmp',nfft);
-    tmp = tmp .* windowFunction';
-    tmp = Utils.bandPassFilter(tmp, fs, 18000,21500);
+    tmp = tmp .* windowFunction';    
+    
+        
+    tmpUp = Utils.bandPassFilter(tmp, fs, 20000,20500);
+    tmpDown = Utils.bandPassFilter(tmp, fs, 19000,19500);
+    
     
     % Get total energy of bandpass filtered signal
-    energy = Utils.getEnergy(tmp);
+    energyUp = Utils.getEnergy(tmpUp);
+    energyDown = Utils.getEnergy(tmpDown);
     
-    % Extract envelopes
-    eUp = Utils.getEnvelope(tmp,upReference);
-    eDown = Utils.getEnvelope(tmp,downReference);
+    % Extract up envelope
+    eUp = Utils.getEnvelope(tmpUp,upReference);    
+    eDown = Utils.getEnvelope(tmpDown,downReference);
     
     %mup = eUp(1);
     %mdown = eDown(1);       
@@ -101,20 +111,28 @@ for i = offset:winSize:length(data)
     mdown = max(eDown);
     
     bit = mup > mdown;
+    bit2 = energyUp > energyDown;
     
-    if energy > 0.02
-       testcode = [testcode; energy, bit];
+    if bit == bit2 & max([energyUp, energyDown]) > 0.1
+        testcode = [testcode; max([energyUp, energyDown]), bit];
     end
+    
+    
+    %   testcode = [testcode; max(energyUp,energyDown), bit];
+    %end
     
     if plotOn > 0
     
         subplot(4,1,1);plot([i:i+nfft-1],eUp,'r-');
         subplot(4,1,2);plot([i:i+nfft-1],eDown,'r-');
     
-        subplot(4,1,4);plot([i,i+nfft-1],[0, energy],'k--');        
+        subplot(4,1,4);plot([i,i+nfft-1],[0, energyUp],'k--');        
+        subplot(4,1,4);plot([i,i+nfft-1],[0, energyDown],'b-.');        
     end
     
 end
+
+%testcode
 
 [~,startIdx] = max(testcode(:,1));
 
